63c4dee24d507f41a4b9b53dbe7c52a01b50a689
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-25 16:14:14 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence1)
-  sentence1.split.collect do |sentence1|
-    sentence1.reverse.join(" ")
+  sentence1.split.collect {|sentence1|sentence1.reverse}.join(" ")
   end
 end
