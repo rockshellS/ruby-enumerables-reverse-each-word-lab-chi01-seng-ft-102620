e91a3970e4f51366de3afec5e5cbf938e8a8e02d
Rachelle A. Spicer <spicer.rachelle@gmail.com>
2020-09-25 15:53:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(string_argument)
-  string_argument.each do |sentence| 
+def reverse_each_word(sentence1)
+  sentence1.each do |sentence| 
     puts "Hello there, and how are you?"
  end
 end
